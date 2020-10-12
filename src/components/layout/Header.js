3f001ef733f5e09373567376c1901082b8743c3d
unknown <rushankshah65@gmail.com>
2020-10-12 18:59:24 +0530

    Refactoring made

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,17 +5,15 @@ const Navbar = () => {
         <div>
             <div className="navbar-fixed">
                 <nav className="purple">
-                    <div className="container">
-                        <div className="nav-wrapper">
-                            <a href="#!" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-                            <a href="#home" className="brand-logo">SEF</a>
-                            <ul className="right hide-on-med-and-down">
-                                <li><a href="sass.html">Sass</a></li>
-                                <li><a href="badges.html">Components</a></li>
-                                <li><a href="collapsible.html">Javascript</a></li>
-                                <li><a href="mobile.html">Mobile</a></li>
-                            </ul>
-                        </div>
+                    <div className="nav-wrapper">
+                        <a href="#!" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                        <a href="#home" className="brand-logo">SEF</a>
+                        <ul className="right hide-on-med-and-down">
+                            <li><a href="sass.html">Sass</a></li>
+                            <li><a href="badges.html">Components</a></li>
+                            <li><a href="collapsible.html">Javascript</a></li>
+                            <li><a href="mobile.html">Mobile</a></li>
+                        </ul>
                     </div>
                 </nav>
             </div>
