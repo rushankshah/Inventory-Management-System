ba911eb58fd432844a346b03d44c24a7786eb886
unknown <rushankshah65@gmail.com>
2020-10-12 18:56:36 +0530

    Warnings resolved

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,46 +1,15 @@
-import React, { useEffect } from 'react'
-import M from 'materialize-css/dist/js/materialize'
+import React from 'react'
 
 const Navbar = () => {
-    useEffect(
-        () => {
-            document.addEventListener('DOMContentLoaded', function () {
-                var elems = document.querySelectorAll('.sidenav');
-            });
-        }
-    )
-    // <div>
-    //     <div className='navbar-fixed'>
-    //         <nav>
-    //             <div class="nav-wrapper">
-    //                 <a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-    //                 <a href="#!" class="brand-logo">Logo</a>
-    //                 <ul class="right hide-on-med-and-down">
-    //                     <li><a href="sass.html">Sass</a></li>
-    //                     <li><a href="badges.html">Components</a></li>
-    //                     <li><a href="collapsible.html">Javascript</a></li>
-    //                     <li><a href="mobile.html">Mobile</a></li>
-    //                 </ul>
-    //             </div>
-    //         </nav>
-    //     </div>
-
-    //     <ul className="sidenav" id="slide-out">
-    //         <li><a href="sass.html">Sass</a></li>
-    //         <li><a href="badges.html">Components</a></li>
-    //         <li><a href="collapsible.html">Javascript</a></li>
-    //         <li><a href="mobile.html">Mobile</a></li>
-    //     </ul>
-    // </div>
     return (
         <div>
-            <div class="navbar-fixed">
-                <nav class="teal">
-                    <div class="container">
-                        <div class="nav-wrapper">
-                            <a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-                            <a href="#home" class="brand-logo">SEF</a>
-                            <ul class="right hide-on-med-and-down">
+            <div className="navbar-fixed">
+                <nav className="purple">
+                    <div className="container">
+                        <div className="nav-wrapper">
+                            <a href="#!" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                            <a href="#home" className="brand-logo">SEF</a>
+                            <ul className="right hide-on-med-and-down">
                                 <li><a href="sass.html">Sass</a></li>
                                 <li><a href="badges.html">Components</a></li>
                                 <li><a href="collapsible.html">Javascript</a></li>
